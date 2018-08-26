c6f0fa6092a0a9176a1931974f9784d3dfbf73fc
Владимир <@DESKTOP-1P2TF6K>
2018-08-26 13:01:10 +0300

    Добавьте файлы проекта.

---
+++ SoccerApp/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoccerApp
+{
+    public partial class Form1 : Form
+    {
+        SoccerContext db;
+        public Form1()
+        {
+            InitializeComponent();
+
+            db = new SoccerContext();
+            db.Players.Load();
+
+            dataGridView1.DataSource = db.Players.Local.ToBindingList();
+        }
+        // добавление
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PlayerForm plForm = new PlayerForm();
+            DialogResult result = plForm.ShowDialog(this);
+
+            if (result == DialogResult.Cancel)
+                return;
+
+            Player player = new Player();
+            player.Age = (int)plForm.numericUpDown1.Value;
+            player.Name = plForm.textBox1.Text;
+            player.Position = plForm.comboBox1.SelectedItem.ToString();
+
+            db.Players.Add(player);
+            db.SaveChanges();
+
+            MessageBox.Show("Новый объект добавлен");
+        }
+        // редактирование
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int index = dataGridView1.SelectedRows[0].Index;
+                int id = 0;
+                bool converted = Int32.TryParse(dataGridView1[0, index].Value.ToString(), out id);
+                if (converted == false)
+                    return;
+
+                Player player = db.Players.Find(id);
+
+                PlayerForm plForm = new PlayerForm();
+
+                plForm.numericUpDown1.Value = player.Age;
+                plForm.comboBox1.SelectedItem = player.Position;
+                plForm.textBox1.Text = player.Name;
+
+                DialogResult result = plForm.ShowDialog(this);
+
+                if (result == DialogResult.Cancel)
+                    return;
+
+                player.Age = (int)plForm.numericUpDown1.Value;
+                player.Name = plForm.textBox1.Text;
+                player.Position = plForm.comboBox1.SelectedItem.ToString();
+
+                db.SaveChanges();
+                dataGridView1.Refresh(); // обновляем грид
+                MessageBox.Show("Объект обновлен");
+
+            }
+        }
+        // удаление
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int index = dataGridView1.SelectedRows[0].Index;
+                int id = 0;
+                bool converted = Int32.TryParse(dataGridView1[0, index].Value.ToString(), out id);
+                if (converted == false)
+                    return;
+
+                Player player = db.Players.Find(id);
+                db.Players.Remove(player);
+                db.SaveChanges();
+
+                MessageBox.Show("Объект удален");
+            }
+        }
+    }
+}
---
+++ SoccerApp/SoccerContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace SoccerApp
+{
+    class SoccerContext : DbContext
+    {
+        public SoccerContext()
+            : base("DefaultConnection")
+        { }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
